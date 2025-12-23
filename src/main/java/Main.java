c107ceaf0902b65f35d29f0545c50ca7edda4be8
Ignacio Gonzalez <ignacio.glezsecadas@gmail.com>
2025-12-23 19:59:37 +0100

    Removed unused imports

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,9 @@
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.lang.reflect.Type;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
